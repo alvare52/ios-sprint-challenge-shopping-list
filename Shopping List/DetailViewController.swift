af321477a9058a57b7c4515642e1a0966922119a
Jorge Alvarez <jorgelalvarez52@gmail.com>
2019-12-20 09:02:29 -0800

    setup alert?

--- Shopping List/DetailViewController.swift
+++ Shopping List/DetailViewController.swift
@@ -17,6 +17,9 @@ class DetailViewController: UIViewController {
     
     @IBAction func sendOrderButton(_ sender: UIButton) {
         print("send order")
+        // Check if text exists and not empty
+        
+        showAlert()
     }
     
     
@@ -28,6 +31,24 @@ class DetailViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    /*
+     In the detail view controller, add a button "Submit Order" that creates a UIAlert saying the user's order will be delivered in 15 minutes to the user's address.
+     
+     The alert must include the user's name in the title
+     The alert must include the address in the message
+
+     */
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "Title with user name", message: "message with address", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: goBackHandler(alert:))) // handler runs after OK
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // handler will run with when OK is pressed (Not required though)
+    func goBackHandler(alert: UIAlertAction!) {
+        navigationController?.popViewController(animated: true)
+    }
 
     /*
     // MARK: - Navigation
